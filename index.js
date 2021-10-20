d1516136ae6cc090f46e1bdb0a857005c7ca74c9
Felicity Brammer <felicitybrammer@gmail.com>
2021-10-20 15:15:50 -0700

    initial files

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please give a detailed description of your project'
+    },
+    {}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
