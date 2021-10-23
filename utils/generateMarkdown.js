43c358d7f771c810d6e625328d4c121608f72728
Felicity Brammer <felicitybrammer@gmail.com>
2021-10-23 14:20:40 -0700

    finalize license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,15 +2,15 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   switch (license) {
-    case 'GNU AGPLv3': 
-    case 'GNU GPLv3':
-    case 'GNU LGPLv3':
-    case 'Mozilla Public License 2.0':
-    case 'Apache License 2.0':
-    case 'MIT License':
-    case 'Boost Software License 1.0':
-    case 'The Unlicense': 
-    default: 
+    case 'GNU AGPLv3': return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)]`;
+    case 'GNU GPLv3': return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`;
+    case 'GNU LGPLv3': return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)]`;
+    case 'Mozilla Public License 2.0': return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]`
+    case 'Apache License 2.0': return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`;
+    case 'MIT License': return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`;
+    case 'Boost Software License 1.0': return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]`;
+    case 'The Unlicense': return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]`;
+    default: return 'None';
   }
 }
 
@@ -18,21 +18,27 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   switch (license) {
-    case 'GNU AGPLv3': 
-    case 'GNU GPLv3':
-    case 'GNU LGPLv3':
-    case 'Mozilla Public License 2.0':
-    case 'Apache License 2.0':
-    case 'MIT License':
-    case 'Boost Software License 1.0':
-    case 'The Unlicense': 
-    default:   
+    case 'GNU AGPLv3': return `https://www.gnu.org/licenses/agpl-3.0`;
+    case 'GNU GPLv3': return `(https://www.gnu.org/licenses/gpl-3.0)`;
+    case 'GNU LGPLv3': return `(https://www.gnu.org/licenses/lgpl-3.0)`;
+    case 'Mozilla Public License 2.0': return `(https://opensource.org/licenses/MPL-2.0)`;
+    case 'Apache License 2.0': return `(https://opensource.org/licenses/Apache-2.0)`;
+    case 'MIT License': return `(https://opensource.org/licenses/MIT)`;
+    case 'Boost Software License 1.0': return `(https://www.boost.org/LICENSE_1_0.txt)`;
+    case 'The Unlicense': return `(http://unlicense.org/)`;
+    default: return 'None';
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  return license; 
+  //???
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -62,7 +68,7 @@ function generateMarkdown(data) {
 
   ## Licenses
 
-  ${data.license}
+  ${renderLicenseSection(data.license)}
 
   ## Contributing
 
